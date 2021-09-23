f1dc3265f3215216f5fc4ab35f5c907ee848d02c
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-09-23 09:39:11 -0300

    Criando o componente sidebar, localizado ao lado do componente story

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,18 @@
 import '../styles/Sidebar.scss'
+import Sticky from 'react-sticky-el'
+import Profile from './Profile'
+import Suggestions from './Suggestions'
+import Footer from './Footer'
+import image from '../images/profile.jpg'
 
 export default function Sidebar() {
-    return <div className="sidebar"></div>
+    return (
+        <Sticky topOffset={-80}>
+            <div className="sidebar">
+                <Profile username="vsg" caption="Descrição" urlText="Switch" iconSize="big" image={image}/>
+                <Suggestions />
+                <Footer />
+            </div>
+        </Sticky>
+        )
 }
---
+++ src/components/Suggestions.js
@@ -0,0 +1,9 @@
+import '../styles/Suggestions.scss'
+
+function Suggestions(){
+    return(
+        <div className="suggestions"></div>
+    )
+}
+
+export default Suggestions;
