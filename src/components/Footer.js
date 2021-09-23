0f15e0e2dee08727cc6447206aeb97855f8e5a78
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-09-23 09:58:15 -0300

    Adicionando footer para a sidebar com copyright e lista desordenada

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,22 @@ import '../styles/Footer.scss'
 
 function Footer(){
     return (
-    <div className="footer"></div>
+    <div className="footer">
+        <ul className="links">
+            <li>About</li>
+            <li>Help</li>
+            <li>Press</li>
+            <li>API</li>
+            <li>Jobs</li>
+            <li>Privacy</li>
+            <li>Terms</li>
+            <li>Locations</li>
+            <li>Top Accounts</li>
+            <li>Hashtags</li>
+            <li>Language</li>
+        </ul>
+        <div className="copyright">@ 2021 - INSTACLONE</div>
+    </div>
     )
 }
 
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,8 +1,20 @@
 import '../styles/Suggestions.scss'
+import Profile from './Profile';
 
 function Suggestions(){
     return(
-        <div className="suggestions"></div>
+        <div className="suggestions">
+            <div className="titleContainer">
+                <div className="title">Sugestões para você</div>
+                <a href="/">Veja tudo</a>
+            </div>
+
+            <Profile caption="Followed by mapvault + 3 more" urlText="Follow" iconSize="medium" captionSize="small" storyBorder={true}/>
+            <Profile caption="Followed by someone + 2 more" urlText="Follow" iconSize="medium" captionSize="small" />
+            <Profile caption="Followed by Hiko + 1 more" urlText="Follow" iconSize="medium" captionSize="small" />
+            <Profile caption="Followed by DoubleD" urlText="Follow" iconSize="medium" captionSize="small" storyBorder={true}/>
+            <Profile caption="Followed by Bugs + 7 more" urlText="Follow" iconSize="medium" captionSize="small" />
+        </div>
     )
 }
 
